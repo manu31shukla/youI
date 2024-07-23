cbe03d2774a8e5f78d22f411721d3e27a2a6a11c
manu31shukla <manu31shukla@gmail.com>
2024-07-23 19:58:42 +0530

    comitting dues

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Page() {
   return (
-      <h1 className="text-5xl text-white"> You_i Application </h1>
+    <div className="w-full h-[100vh] flex justify-center items-center text-center">
+      <h1 className="text-2xl text-white"> You_i Application </h1>
+    </div>
   );
 }
